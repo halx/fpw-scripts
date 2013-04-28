1cbfea5a6e4d6c6818c4c76dc19dcdd4d6c77a49
Hannes.Loeffler@gmail.com <Hannes.Loeffler@gmail.com@d0e4893c-28a2-d7ce-a480-37206996e6b7>
2013-04-28 11:43:40 +0000

    single script for conversion control

--- JMdict/convert.pl
+++
@@ -1,9 +0,0 @@
-my $error = 0;
-
-foreach my $lang (@JMdict_lang) {
-  $ENV{'DICT_LANG'} = $lang;
-
-  $error = buildall();
-}
-
-!$error;
--- kanjidic/convert.pl
+++
@@ -1,9 +0,0 @@
-my $error = 0;
-
-foreach my $lang (@kanjidic_lang) {
-  $ENV{'DICT_LANG'} = $lang;
-
-  $error = buildall();
-}
-
-!$error;
